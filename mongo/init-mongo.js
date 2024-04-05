eb6bef62990d2cfcb836687ca25299677e042d42
Mariia Nesterenko <marynestes@gmail.com>
2024-04-05 12:36:31 +0700

    add gitaction

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -8,3 +8,17 @@ db.createCollection('activities');
 db.createCollection('reviews');
 
 
+db.users.insertMany([
+    {
+      fullname: "jane doe",
+      email: "jane@example.com",
+      disabled: null,
+      password: "$argon2id$v=19$m=65536,t=3,p=4$snYOwXhv7b3XmvN+z3nvXQ$BNZz1qvFFmKsB+ZxyhbLh6h7+7+0WUjeSnz65uJ9TTU"
+    },
+    {
+      fullname: "alex smith",
+      email: "alex@example.com",
+      disabled: null,
+      password: "$argon2id$v=19$m=65536,t=3,p=4$snYOwXhv7b3XmvN+z3nvXQ$BNZz1qvFFmKsB+ZxyhbLh6h7+7+0WUjeSnz65uJ9TTU"
+    }
+  ]);
